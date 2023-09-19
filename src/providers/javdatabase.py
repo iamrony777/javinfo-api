1e5dbe35a713ddb5bccba74801be4740a0ee69ab
Ronit Sarkar <iamrony777@proton.me>
2023-09-19 14:08:37 +0000

    Tue, Sep 19, 2023, 2:08 PM +00:00

--- src/providers/javdatabase.py
+++ src/providers/javdatabase.py
@@ -3,9 +3,23 @@ javdatabase.com scrapper
 Author @github.com/iamrony777
 """
 
-from typing import Optional
+from httpx import AsyncClient
 
 class Javdatabase:
-    def __init__(self, base_url: Optional[str] ) -> None:
+    def __init__(self, base_url: str = "https://www.javdatabase.com" ) -> None:
+        self.base_url = base_url
+        self.headers = {
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.101 Safari/537.36",
+            "Accept": "*/*",
+        }
 
-        base
+        self.client = AsyncClient(
+            base_url=self.base_url,
+            headers=self.headers,
+            http2=True,
+            follow_redirects=True,
+            timeout=5,
+        )
+
+javdatabase = Javdatabase()
+print(javdatabase.base_url)
