7fcc9e3e196eb33994f422dde793e9aaf6b10a74
Ronit Sarkar <iamrony777@proton.me>
2023-09-19 14:02:34 +0000

    Tue, Sep 19, 2023, 2:02 PM +00:00

---
+++ src/providers/javdatabase.py
@@ -0,0 +1,10 @@
+"""
+javdatabase.com scrapper
+Author @github.com/iamrony777
+"""
+
+from typing import Optional
+
+class Javdatabase:
+    def __init__(self, base_url: Optional[str] ) -> None:
+        
