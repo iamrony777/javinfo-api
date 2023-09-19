954c28323ffca0f51fde9ed6d459960098ee6e31
Ronit Sarkar <iamrony777@proton.me>
2023-09-19 15:56:55 +0000

    Tue, Sep 19, 2023, 9:26 PM +05:30

--- src/providers/javdatabase.py
+++ src/providers/javdatabase.py
@@ -7,7 +7,7 @@ from httpx import AsyncClient
 
 
 class Javdatabase:
-    def __init__(self, base_url: str = "https://www.javdatabase.com") -> None:
+    def __init__(self, base_url: str = "https://www.javlibrary.com/en/") -> None:
         self.base_url = base_url
         self.headers = {
             "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.101 Safari/537.36",
@@ -25,7 +25,8 @@ class Javdatabase:
     async def search(self, code: str):
         """public method: search"""
         # code = code.lower()
-        resp = await self.client.get(f"movies/{code.lower()}")
+        # resp = await self.client.get(f"movies/{code.lower()}")
+        resp = await self.client.get("/")
         if resp.status_code != 200:
             return {"statusCode": resp.status_code}
         else:
