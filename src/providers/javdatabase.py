cf69b076df5827b396932c83671cde4c007849ad
Ronit Sarkar <iamrony777@proton.me>
2023-09-20 03:16:50 +0000

    Wed, Sep 20, 2023, 8:46 AM +05:30

--- src/providers/javdatabase.py
+++ src/providers/javdatabase.py
@@ -3,25 +3,31 @@ javdatabase.com scrapper
 Author @github.com/iamrony777
 """
 
-from cloudscraper import create_scraper
 from urllib.parse import urljoin
+from cloudscraper import create_scraper
 
 
 class Javdatabase:
-    def __init__(self, base_url: str = "https://jav") -> None:
+    def __init__(self, base_url: str = "https://javdatabase.com/") -> None:
         self.base_url = base_url
         self.headers = {
             "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.101 Safari/537.36",
             "Accept": "*/*",
         }
-        self.client = create_scraper(browser={"browser": "chrome", "platform": "linux", "desktop": True})
+        self.client = create_scraper(
+            browser={"browser": "chrome", "platform": "linux", "desktop": True}
+        )
 
     async def search(self, code: str):
         """public method: search"""
         # code = code.lower()
         # resp = await self.client.get(f"movies/{code.lower()}")
-        resp = await self.client.get(urljoin(base=self.base_url, url=f"movies/{code.lower()}"))
-        if resp.status_code != 200:
+        resp = self.client.get(
+            urljoin(base=self.base_url, url=f"movies/{code.lower()}"),
+            allow_redirects=True,
+            timeout=5,
+        )
+        if not resp.ok:
             return {"statusCode": resp.status_code}
         else:
             return resp.text
