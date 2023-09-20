299b4ceea50039751bc7fe27f846cbe41c9bf63f
Ronit Sarkar <iamrony777@proton.me>
2023-09-20 07:38:48 +0000

    ddd, Sep 9/20/2023, YYYY, 1:08 A +5:30

---
+++ src/tests/javdatabase.py
@@ -0,0 +1,24 @@
+import pytest
+from src.providers import Javdatabase
+
+# Define the codes to test
+codes = ["ebod-391", "mkck-275", "doa-017"]
+provider = Javdatabase()
+
+
+@pytest.mark.parametrize("code", codes)
+def test_javdatabase(code:str):
+    # Load the expected result from the json file
+    with open(f"data/javdatabase.{code}.json", "r") as f:
+        expected_result: str = f.read().strip()
+
+    # Run the search function and get the result
+    result = provider.search(code.upper()).strip()
+
+    # Compare the result with the expected result
+    assert (
+        result == expected_result
+    ), f"For code: {code}, expected: {expected_result}, but got: {result}"
+
+
+pytest.main(["-x", __file__])
