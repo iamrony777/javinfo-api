b691ef9e8add527d3789890234f4a503d5105fd5
Ronit Sarkar <iamrony777@proton.me>
2023-09-20 19:59:06 +0000

    ddd, Sep 9/21/2023, YYYY, 1:29 A +5:30

--- src/providers/__init__.py
+++ src/providers/__init__.py
@@ -1,1 +1,14 @@
 from .javdatabase import Javdatabase
+
+
+from requests import Session
+from urllib.parse import urljoin
+
+class CustomSession(Session):
+    def __init__(self, base_url=None):
+        super().__init__()
+        self.base_url = base_url
+
+    def request(self, method, url, *args, **kwargs):
+        joined_url = urljoin(self.base_url, url)
+        return super().request(method, joined_url, *args, **kwargs)
--- src/providers/r18.py
+++ src/providers/r18.py
@@ -1,12 +1,13 @@
 import requests
-
+from providers import CustomSession
 
 class R18:
     def __init__(self, base_url: str = "https://r18.dev") -> None:
         self.base_url = base_url
-        self.client = requests.Session()
+        self.client = CustomSession(base_url=base_url)
 
     def __getJsonResult(self):
         pass
 
     def search(self, code:str):
+        self.client.get(url=f"/videos/vod/movies/detail/-/dvd_id=${code}/json")
