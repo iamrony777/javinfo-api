e76b6a51612a89b4e5ea16ed943b2e706f2ec15b
Ronit Sarkar <iamrony777@proton.me>
2023-09-21 04:17:28 +0000

    ddd, Sep 9/21/2023, YYYY, 9:47 A +5:30

--- src/providers/javlibrary.py
+++ src/providers/javlibrary.py
@@ -1,6 +1,7 @@
 from urllib.parse import urljoin
 from cloudscraper import create_scraper
 
+
 class Javlibrary:
     def __init__(self, base_url: str = "https://www.javlibrary.com/en/") -> None:
         self.base_url = base_url
@@ -8,6 +9,16 @@ class Javlibrary:
             browser={"browser": "chrome", "platform": "linux", "desktop": True}
         )
 
-    def search(self, code:str):
+    def search(self, code: str):
         # first search for checking availability
-        
+        resp = self.client.get(
+            url=urljoin(base=self.base_url, url="vl_searchbyid.php"),
+            params={"keyword": code.upper()},
+            allow_redirects=True,
+        )
+        print({"statusCode": resp.status_code, "url": resp.url})
+        return resp.ok
+
+
+if __name__ == "__main__":
+    print(Javlibrary().search("SSIS-001"))
