a493532859fb06c05a92572e236b7ac89d0fb3b8
Ronit Sarkar <iamrony777@proton.me>
2023-09-21 12:41:23 +0000

    ddd, Sep 9/21/2023, YYYY, 6:11 A +5:30

--- src/___init__.py
+++ src/___init__.py
@@ -1,5 +1,25 @@
-from .providers import Javdatabase, R18, Javlibrary
+from src.providers import Javdatabase, R18, Javlibrary
+import concurrent.futures
 
 r18Provider = R18()
 jvdtbsProvider = Javdatabase()
 jvlibProvideer = Javlibrary()
+
+
+
+r18Provider = R18()
+jvdtbsProvider = Javdatabase()
+jvlibProvideer = Javlibrary()
+
+def search_all_providers(code: str):
+    providers = [r18Provider, jvdtbsProvider, jvlibProvideer]
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = [executor.submit(provider.search, code) for provider in providers]
+        results = [future.result() for future in concurrent.futures.as_completed(futures)]
+
+    return min(results, key=lambda result: result)
+
+
+if __name__ == "__main__":
+    print(search_all_providers('EBOD-391'))
