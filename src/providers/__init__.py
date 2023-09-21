4f2bc12b8805d2acfb2a01b9701a1255507da3d9
Ronit Sarkar <iamrony777@proton.me>
2023-09-21 12:06:32 +0000

    ddd, Sep 9/21/2023, YYYY, 5:36 A +5:30

--- src/providers/__init__.py
+++ src/providers/__init__.py
@@ -1,2 +1,3 @@
 from .javdatabase import Javdatabase
 from .r18 import R18
+from .javlibrary import Javlibrary
---
+++ src/tests/javlibrary.py
@@ -0,0 +1,25 @@
+import pytest
+import json
+from src.providers import Javlibrary
+
+# Define the codes to test
+codes = ["ebod-391", "ssis-100", "doa-017", "ssis-001"]
+provider = Javlibrary()
+
+
+@pytest.mark.parametrize("code", codes)
+def test_javlibrary(code:str):
+    # Load the expected result from the json file
+    with open(f"data/javlibrary.{code}.json", "r") as f:
+        expected_result: str = f.read().strip()
+
+    # Run the search function and get the result
+    result = json.dumps(provider.search(code.upper()), indent=2, ensure_ascii=False)
+
+    # Compare the result with the expected result
+    assert (
+        result == expected_result
+    ), f"For code: {code}, expected: {expected_result}, but got: {result}"
+
+
+# pytest.main(["-x", __file__])
