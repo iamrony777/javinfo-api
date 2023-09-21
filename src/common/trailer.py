afbb6d5441e3994a68709f4d99bda04cf37c1e9b
Ronit Sarkar <iamrony777@proton.me>
2023-09-21 06:52:16 +0000

    ddd, Sep 9/21/2023, YYYY, 12:22 A +5:30

--- src/common/trailer.py
+++ src/common/trailer.py
@@ -2,6 +2,19 @@
 fetch trailer/preview from https://www.dmm.co.jp/service/-/html5_player/
 """
 
+from urllib.parse import urljoin
 from lxml import html
+from requests import request
 
-def getPreview(code:str)
+
+def getPreview(code: str):
+    return request(
+        method="GET",
+        url=urljoin(
+            base="https://www.dmm.co.jp/service/-/html5_player",
+            url=f"/=/cid={code}/mtype=AhRVShI_/service=mono/floor=dvd/mode=/",
+        ),
+    ).content
+
+if __name__ == "__main__":
+    print(getPreview("mkck275"))
