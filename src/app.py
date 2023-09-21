9116af7eaa10a227b8f8e7a7da72b4ed96314a69
Ronit Sarkar <iamrony777@proton.me>
2023-09-21 12:26:48 +0000

    ddd, Sep 9/21/2023, YYYY, 5:56 A +5:30

--- src/app.py
+++ src/app.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+from fastapi.requests import Request
 
 app = FastAPI()
 
+
 @app.get("/")
-async def root(request):
-    return {"hello": request}
+async def root(request: Request):
+    return {"hello": request.headers.get("X-Forwarded-From")}
