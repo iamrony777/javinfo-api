2c67a216b15d0b0a29216dbf2d05ed6495aebe4a
Ronit Sarkar <iamrony777@proton.me>
2023-09-21 04:53:22 +0000

    ddd, Sep 9/21/2023, YYYY, 10:23 A +5:30

--- src/providers/javlibrary.py
+++ src/providers/javlibrary.py
@@ -22,14 +22,24 @@ class Javlibrary:
         if resp.ok and bool(
             re.search(pattern=r"\?keyword=[a-zA-Z0-9]+", string=resp.url)
         ):  # duplicate or no results found
-            page: html.HtmlElement = html.fromstring(html=resp.content, base_url=self.base_url)
-            try: ## No result found
-                return { "statusCode": 404, "error": page.cssselect("#rightcolumn > p > em")[0].text}
-            except IndexError: ## Duplicate/Many results
+            page: html.HtmlElement = html.fromstring(
+                html=resp.content, base_url=self.base_url
+            )
+            try:  ## No result found
+                return {
+                    "statusCode": 404,
+                    "error": page.cssselect("#rightcolumn > p > em")[0].text,
+                }
+            except IndexError:  ## Duplicate/Many results
                 for each in page.cssselect("div.videothumblist > div.videos > div"):
-                    if code == each.find('a/div').text:
-                        url = urljoin(base=self.base_url, url=each.find("a").get("href"))
-                return { "url": resp.url }
+                    if code == each.find("a/div").text:
+                        resp = self.client.get(
+                            url=urljoin(
+                                base=self.base_url, url=each.find("a").get("href")
+                            ),
+                            allow_redirects=True,
+                        )
+                        return
 
         elif resp.ok and bool(
             re.search(pattern=r"\?v=[a-zA-Z0-9]+", string=resp.url)
