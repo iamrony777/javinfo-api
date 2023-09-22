74b3dea75779210e9b44c0c556e290ff9aa0f1b2
Ronit Sarkar <iamrony777@proton.me>
2023-09-22 12:19:32 +0000

    ddd, Sep 9/22/2023, YYYY, 5:49 A +5:30

--- src/___init__.py
+++ src/___init__.py
@@ -5,21 +5,4 @@ r18Provider = R18()
 jvdtbsProvider = Javdatabase()
 jvlibProvideer = Javlibrary()
 
-
-
-r18Provider = R18()
-jvdtbsProvider = Javdatabase()
-jvlibProvideer = Javlibrary()
-
-def search_all_providers(code: str):
-    providers = [r18Provider, jvdtbsProvider, jvlibProvideer]
-
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        futures = [executor.submit(provider.search, code) for provider in providers]
-        results = [future.result() for future in concurrent.futures.as_completed(futures)]
-
-    return min(results, key=lambda result: result)
-
-
-if __name__ == "__main__":
-    print(search_all_providers('EBOD-391'))
+#TODO: all of these providers has a common method `search`. Now create a function which searches all of them using python's co
