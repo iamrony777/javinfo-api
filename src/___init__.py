0ffedac8d47875e029c7736192770954348f882c
Ronit Sarkar <iamrony777@proton.me>
2023-09-22 12:21:56 +0000

    ddd, Sep 9/22/2023, YYYY, 5:51 A +5:30

--- src/___init__.py
+++ src/___init__.py
@@ -5,4 +5,24 @@ r18Provider = R18()
 jvdtbsProvider = Javdatabase()
 jvlibProvideer = Javlibrary()
 
-#TODO: all of these providers has a common method `search`. Now create a function which searches all of them using python's co
+#TODO: all of these providers has a common method `search`. Now create a function which searches all of them using python's `concurrent.futures`. and make the logic like this, whichever provider returns the result first, then return it. dont wait for other providers to finish. if this is not possible with `concurrent.futures` then you're free to use other libraries.
+
+def search_all_providers():
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        # Submit the search tasks to the executor
+        r18_search = executor.submit(r18Provider.search)
+        jvdtbs_search = executor.submit(jvdtbsProvider.search)
+        jvlib_search = executor.submit(jvlibProvideer.search)
+
+        # Wait for any of the tasks to complete
+        completed, _ = concurrent.futures.wait(
+            [r18_search, jvdtbs_search, jvlib_search],
+            return_when=concurrent.futures.FIRST_COMPLETED
+        )
+
+        # Get the result from the completed task
+        for task in completed:
+            return task.result()
+
+# Call the function to perform the search
+result = search_all_providers()
